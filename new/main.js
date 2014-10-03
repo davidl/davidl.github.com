060824d193381501fa4411c4ffd6356437160ab7
davidl <davidlantner@gmail.com>
2014-10-02 20:12:34 -0400

    handle window resize

--- new/main.js
+++ new/main.js
@@ -1,31 +1,42 @@
-(function(w, d) {
-	// Expand sections to fill window:
-	var e = d.documentElement,
-	    g = d.getElementsByTagName('body')[0],
-	    x = w.innerWidth || e.clientWidth || g.clientWidth,
-	    y = w.innerHeight|| e.clientHeight|| g.clientHeight,
-		cards = d.querySelectorAll('header, article');
-	for ( var i = 0; i < cards.length; i++ ) {
-		cards[i].style.height = y + 'px';
-	}
-	
-	var slideLinks = d.querySelectorAll('.slide');
-	for ( var i = 0; i < slideLinks.length; i++ ) {
-	    slideLinks[i].addEventListener('click', function(e) {
-	        e.preventDefault();
-	        var targetEl = d.getElementById(e.currentTarget.hash.substr(1));
-	        Velocity(targetEl, 'scroll', {
-	            duration: 600,
-	            easing: 'ease',
-	            container: d.querySelector('.wrapper'),
-	            complete: function(elements) {
-                    // To Do: send focus to target element - something like this (but working):
-                    // d.querySelector('a', targetEl).focus();
-	            }
-	        });
-	    }, false);
-	}
+var ddom = (function(w, d){
+    function setCardHeights() {
+        var e = d.documentElement,
+            g = d.getElementsByTagName('body')[0],
+            x = w.innerWidth || e.clientWidth || g.clientWidth,
+            y = w.innerHeight|| e.clientHeight|| g.clientHeight,
+            cards = d.querySelectorAll('header, article');
+        for ( var i = 0; i < cards.length; i++ ) {
+            cards[i].style.height = y + 'px';
+        }
+    }
+
+    function animateSlideLinks() {
+        var slideLinks = d.querySelectorAll('.slide');
+        for ( var i = 0; i < slideLinks.length; i++ ) {
+            slideLinks[i].addEventListener('click', function(e) {
+                e.preventDefault();
+                var targetEl = d.getElementById(e.currentTarget.hash.substr(1));
+                Velocity(targetEl, 'scroll', {
+                    duration: 600,
+                    easing: 'ease',
+                    container: d.querySelector('.wrapper'),
+                    complete: function(elements) {
+                        // To Do: use Velocity to animate elements
+                    }
+                });
+            }, false);
+        }
+    }
+
+    return {
+        setCardHeights: setCardHeights,
+        animateSlideLinks: animateSlideLinks
+    }
 })(window, document);
+
+ddom.setCardHeights();
+window.addEventListener('resize', ddom.setCardHeights, false);
+ddom.animateSlideLinks();
 
 var _gaq=[['_setAccount','UA-40086270-1'],['_trackPageview']];
 (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];g.async=1;
