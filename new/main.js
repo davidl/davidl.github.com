e96e510d8caa864205c0f86787afd0cca64dde4d
davidl <davidlantner@gmail.com>
2014-10-22 21:14:48 -0400

    don't bind to resize

--- new/main.js
+++ new/main.js
@@ -1,44 +1,53 @@
-var ddom = (function(w, d){
-    function setCardHeights() {
-        var e = d.documentElement,
-            g = d.getElementsByTagName('body')[0],
-            x = w.innerWidth || e.clientWidth || g.clientWidth,
-            y = w.innerHeight|| e.clientHeight|| g.clientHeight,
-            cards = d.querySelectorAll('header, article');
-        for ( var i = 0; i < cards.length; i++ ) {
-            cards[i].style.height = y + 'px';
+/*global Velocity*/
+(function () {
+    'use strict';
+    var ddom = (function (w, d) {
+        function setCardHeights() {
+            var e = d.documentElement,
+                g = d.getElementsByTagName('body')[0],
+                x = w.innerWidth || e.clientWidth || g.clientWidth,
+                y = w.innerHeight || e.clientHeight || g.clientHeight,
+                cards = d.querySelectorAll('header, article'),
+                i;
+            for (i = 0; i < cards.length; i++) {
+                cards[i].style.height = y + 'px';
+            }
         }
-    }
 
-    function animateSlideLinks() {
-        var slideLinks = d.querySelectorAll('.slide');
-        for ( var i = 0; i < slideLinks.length; i++ ) {
-            slideLinks[i].addEventListener('click', function(e) {
-                e.preventDefault();
-                var targetEl = d.getElementById(e.currentTarget.hash.substr(1));
-                Velocity(targetEl, 'scroll', {
-                    duration: 600,
-                    easing: 'ease',
-                    container: d.querySelector('.wrapper'),
-                    complete: function(elements) {
-                        // To Do: use Velocity to animate elements
-                    }
-                });
-            }, false);
+        function animateSlideLinks() {
+            var slideLinks = d.querySelectorAll('.slide'),
+                i = 0;
+            for (; i < slideLinks.length; i++) {
+                slideLinks[i].addEventListener('click', function(e) {
+                    e.preventDefault();
+                    var targetEl = d.getElementById(e.currentTarget.hash.substr(1));
+                    Velocity(targetEl, 'scroll', {
+                        duration: 600,
+                        easing: 'ease',
+                        container: d.querySelector('.wrapper'),
+                        complete: function(elements) {
+                            // To Do: use Velocity to animate elements
+                            // of each portfolio item
+                        }
+                    });
+                }, false);
+            }
         }
-    }
+        
+        var returnObject = {
+            setCardHeights: setCardHeights,
+            animateSlideLinks: animateSlideLinks
+        }
 
-    return {
-        setCardHeights: setCardHeights,
-        animateSlideLinks: animateSlideLinks
-    }
-})(window, document);
+        return returnObject;
+    })(window, document);
 
-ddom.setCardHeights();
-window.addEventListener('resize', ddom.setCardHeights, false);
-ddom.animateSlideLinks();
+    ddom.setCardHeights();
+    //window.addEventListener('resize', ddom.setCardHeights, false);
+    ddom.animateSlideLinks();
 
-var _gaq=[['_setAccount','UA-40086270-1'],['_trackPageview']];
-(function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];g.async=1;
-g.src='//www.google-analytics.com/ga.js';
-s.parentNode.insertBefore(g,s)}(document,'script'));
+    var _gaq=[['_setAccount','UA-40086270-1'],['_trackPageview']];
+    (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];g.async=1;
+    g.src='//www.google-analytics.com/ga.js';
+    s.parentNode.insertBefore(g,s)}(document,'script'));
+})();
